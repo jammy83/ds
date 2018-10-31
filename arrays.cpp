31e8759bffa006fbdab7b3883001dbe2067e8a8d
jammy83 <jammy83@users.noreply.github.com>
2018-10-31 07:24:58 -0700

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -589,7 +589,7 @@ public:
 class Solution {
 public:
     int getMid(int start, int end) {
-        return start + ((end - start) / 2);
+        return start + (end - start) / 2;
     }
     int BST(vector<int>& nums, int start, int end, int target) {
         if (start > end) {
@@ -605,6 +605,29 @@ public:
         }
     }
 }
+
+// Find the first occurrence of an element in a sorted array (with duplicates)
+class Solution {
+public:
+    int getMid(int start, int end) {
+        return start + (end - start) / 2;
+    }
+    int searchFirstOfK(const vector<int>& nums, int k) {
+        int start = 0, end = nums.size()-1, result = -1;
+        while (start <= end) {
+            int mid = getMid(start, end);
+            if (k < nums[mid]) {
+                end = mid - 1;
+            } else if (k == nums[mid]) {
+                result = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;    
+            }
+        }
+        return result;
+    }
+};
 
 // 26: Search for an element in a sorted, rotated array with no duplicates
 class Solution {
