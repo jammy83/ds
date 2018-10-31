6a269dcfd6e09424f2392efd5e6f5d156db50614
jammy83 <jammy83@users.noreply.github.com>
2018-10-31 16:27:03 -0700

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -647,6 +647,25 @@ public:
     }
 };
 
+// search a sorted array of distinct integers for an element equal to its index
+class Solution {
+public:
+    int searchEntryEqualToIndex(vector<int>& nums, int k) {
+        int start = 0, end = nums.size() - 1, result = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > mid) {
+                end = mid - 1;
+            } else if (nums[mid] == mid) {
+                return mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+};
+
 // 26: Search for an element in a sorted, rotated array with no duplicates
 class Solution {
 public:
