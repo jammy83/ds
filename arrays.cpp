724e476df210504d3bf84be234e971f71e8cc97c
jammy83 <jammy83@users.noreply.github.com>
2018-10-31 15:52:16 -0700

    Update arrays.cpp

--- arrays.cpp
+++ arrays.cpp
@@ -629,6 +629,24 @@ public:
     }
 };
 
+// search a sorted array for the first element greater than given value
+class Solution {
+public:
+    int searchFirstLargerOfK(vector<int>& nums, int k) {
+        int start = 0, end = nums.size()-1, result = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > k) {
+                result = mid;
+                end = mid - 1;    
+            } else {
+                start = mid + 1;
+            }
+        }
+        return result;
+    }
+};
+
 // 26: Search for an element in a sorted, rotated array with no duplicates
 class Solution {
 public:
