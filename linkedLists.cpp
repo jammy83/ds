ae92c5d77ed4408f6f27b15b691eee3ba8de40ef
jammy83 <jammy83@users.noreply.github.com>
2019-07-17 21:00:15 -0700

    Update linkedLists.cpp

--- linkedLists.cpp
+++ linkedLists.cpp
@@ -388,15 +388,12 @@ public:
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        if (head == nullptr || head-> next == nullptr) {
-            return head;
-        }
-        ListNode *slow = head, *fast = head->next->next;
-        while (fast != nullptr && fast->next != nullptr) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
         }
-        return slow->next;
+        return slow;
     }
 };
 
