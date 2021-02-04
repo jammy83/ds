8c68b5064e1def90f698eedaa0368c648eafbfd5
jammy83 <jammy83@users.noreply.github.com>
2021-02-04 00:05:37 -0800

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,47 @@
+//Set Matrix zeroes
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool rowZ = false, colZ = false;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0)
+                        rowZ = true;
+                    if (j == 0)
+                        colZ = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.size(); i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < matrix[0].size(); j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        for (int j = 1; j < matrix[0].size(); j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < matrix.size(); i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (rowZ) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (colZ) {
+            for (int i = 0; i < matrix.size(); i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
 //Reverse words in a string III
 class Solution {
 public:
