62f2dae06487a0d98e659dca2d775685fc8aa82e
jammy83 <jammy83@users.noreply.github.com>
2021-02-09 16:08:23 -0800

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,31 @@
+//Palindromic substrings
+class Solution {
+public:
+    int countSubstrings(string s) {
+        if (s.empty())
+            return 0;
+        int n, count;
+        n = count = s.length();
+        bool dp[n][n];
+        fill_n(*dp, n * n, false);
+        // Base case: single letter substrings
+        for (int i = 0; i < n; ++i)
+            dp[i][i] = true;
+        // Base case: double letter substrings
+        for (int i = 0; i < n - 1; ++i) {
+            dp[i][i + 1] = (s[i] == s[i + 1]);
+            count += dp[i][i + 1];
+        }
+        // All other cases: substrings of length 3 to n
+        for (int len = 3; len <= n; ++len)
+            for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
+                dp[i][j] = dp[i + 1][j - 1] && (s[i] == s[j]);
+                count += dp[i][j];
+            }
+        return count;
+    }
+};
+
 //Valid Palindrome - II
 class Solution {
 public:
