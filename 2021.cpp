8e11cd34596e0b1793ac6b0710a7c1f1d93dbe1b
jammy83 <jammy83@users.noreply.github.com>
2021-02-11 12:03:41 -0800

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,23 @@
+//Simple BST to search for a number in a given array
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return searchTarget(nums, target, 0, nums.size()-1);
+    }
+    int searchTarget(vector<int>& nums, int target, int low, int high) {
+        if (low > high)
+            return -1;
+        int mid = (low + high) / 2;
+        if (target == nums[mid]) {
+            return mid;
+        } else if (target < nums[mid]) {
+            return searchTarget(nums, target, low, mid-1);
+        } else {
+            return searchTarget(nums, target, mid+1, high);
+        }
+    }
+};
+
 //Max consecutive ones II - flip atmost one of the zeroes to get the max consecutive ones
 #include <algorithm>
 class Solution {
