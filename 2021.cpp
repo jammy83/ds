d733b9c0800acf3b12b6d199f00c2e1197820c51
jammy83 <jammy83@users.noreply.github.com>
2021-02-12 10:00:16 -0800

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,37 @@
+//Search in a 2d matrix with sorted rows 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int low = 0, high = matrix.size()-1, n = matrix[0].size();
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            if (matrix[mid][0] == target || matrix[mid][n-1] == target)
+                return true;
+            if (matrix[mid][0] < target) {
+                if (target < matrix[mid][n-1])
+                    return BS(matrix, mid, target);
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+        return false;
+    }
+    bool BS(vector<vector<int>>& matrix, int row, int target) {
+        int low = 0, high = matrix[0].size()-1;
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            if (matrix[row][mid] == target)
+                return true;
+            if (matrix[row][mid] < target)
+                low = mid + 1;
+            else 
+                high = mid - 1;
+        }
+        return false;
+    }
+};
+
 //SQRT of a number - BST problem : when looking for an integer whose square is close to the number given
 class Solution {
 public:
