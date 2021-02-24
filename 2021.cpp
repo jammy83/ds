42d3729e21e4c93bf77cde58d99ce2a653e81a28
jammy83 <jammy83@users.noreply.github.com>
2021-02-24 12:16:57 -0800

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,27 @@
+//Intersection of 2 arrays
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> result;
+        for (int i = 0, j = 0; i < nums1.size() && j < nums2.size(); ) {
+            if (nums1[i] == nums2[j]) {
+                int val = nums1[i];
+                result.push_back(val);                
+                while (i < nums1.size() && nums1[i] == val)
+                    i++;
+                while (j < nums2.size() && nums2[j] == val)
+                    j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else
+                j++;
+        }
+        return result;
+    }
+};
+
 //Given a static sorted array with unknown size, search for a target value and return its index
 class Solution {
 public:
