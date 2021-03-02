dec479dfed7adc41ae2364535fb0c257cd74ddf2
jammy83 <jammy83@users.noreply.github.com>
2021-03-01 16:37:14 -0800

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,21 @@
+//valid anagram
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+        unordered_multiset<char> s1;
+        for (char c : s)
+            s1.insert(c);
+        for (char c : t) {
+            auto itr = s1.find(c);
+            if (itr != s1.end())
+                s1.erase(itr);
+        }
+        return s1.size()==0;
+    }
+};
+
 //Isomorphic
 class Solution {
 public:
