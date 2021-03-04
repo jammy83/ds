91d37e548d56d7a98314ef9337339f59dd0d4ec8
jammy83 <jammy83@users.noreply.github.com>
2021-03-04 15:12:22 -0800

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,17 @@
+//Find the Kth largest element in an array
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int, vector<int>, greater<int>> q; //min-heap
+        for (int num : nums) {
+            q.push(num);
+            if (q.size() > k)
+                q.pop();
+        }
+        return q.top();
+    }
+};
+
 //Kth largest element in a stream
 class KthLargest {
 public:
