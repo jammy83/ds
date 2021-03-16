3109b9399f025efdc9f24538b6f6d1b1dc17234e
jammy83 <jammy83@users.noreply.github.com>
2021-03-16 13:02:49 -0700

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,24 @@
+//valid parentheses
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> p;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[')
+                p.push(c);
+            else if (c == ')' || c == '}' || c == ']') {
+                if (p.empty())
+                    return false;
+                char t = p.top();
+                if ((c == ')' && t != '(') || (c == '}' && t != '{') || (c == ']' && t != '['))
+                    return false;
+                p.pop();
+            }
+        }
+        return p.empty();
+    }
+};
+
 //Max queue
 class MaxQueue {
 public:
