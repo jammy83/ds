5ac7d45c2a6bc5c866440c1e9da1e4f5cc94e051
jammy83 <jammy83@users.noreply.github.com>
2021-03-20 17:37:30 -0700

    Update 2021.cpp

--- 2021.cpp
+++ 2021.cpp
@@ -1,3 +1,20 @@
+//Meeting rooms
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        if (intervals.empty())
+            return true;
+        sort(intervals.begin(), intervals.end(),
+             [](const vector<int>& a, const vector<int>&b) {
+                return a[0] < b[0]; });
+        for (int i = 0; i < intervals.size()-1; i++) {
+            if (intervals[i][1] > intervals[i+1][0])
+                return false;
+        }
+        return true;
+    }
+};
+
 //Insert an interval and make all to be non-overlapping
 class Solution {
 public:
